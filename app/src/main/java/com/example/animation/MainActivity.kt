7e0fb5ff98af87b8834b41e1d0ea4e63a65987e6
Aurora0043 <940403army@gmail.com>
2024-12-02 15:31:05 +0800

    v2

--- app/src/main/java/com/example/animation/MainActivity.kt
+++ app/src/main/java/com/example/animation/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
@@ -27,10 +29,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -59,13 +64,19 @@ fun Animation(m: Modifier) {
         if (appear) 360f else 0f,
         animationSpec = tween(durationMillis = 2500)
     )
+    val backgroundColor by animateColorAsState(
+        if (appear) Color.Transparent else Color.Green,
+        animationSpec = tween(2000, 500)
+    )
+    val rocketSize by animateDpAsState(
+        if (fly) 75.dp else 150.dp,
+        animationSpec = tween(2000)
+    )
 
-
-    Column {
+    Column (Modifier.background(backgroundColor)){
         Button(
             onClick = { appear = !appear },
             modifier = Modifier.rotate(buttonAngle)
-            modifier = m
         ) {
             if (appear) Text(text = "星空背景圖消失")
             else Text(text = "星空背景圖出現")
@@ -97,6 +108,16 @@ fun Animation(m: Modifier) {
                     .height(if (expanded) 600.dp else 400.dp)
                     .clickable() {
                         expanded = !expanded
+                    }
+            )
+            Image(
+                painter = painterResource(id = R.drawable.rocket),
+                contentDescription = "火箭",
+                modifier = Modifier
+                    .size(rocketSize)
+                    .clickable(
+                    ) {
+                        fly = !fly
                     }
             )
         }
